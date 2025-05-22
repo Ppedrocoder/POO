cf92e05671003c23abe67a1dfedfca73be57692e
Ppedrocoder <rpedroricardo87@gmail.com>
2025-05-22 17:27:40 +0000

    supermercado

---
+++ supermercado.java
@@ -0,0 +1,17 @@
+public class supermercado {
+    private String[] pedido;
+    private String item;
+    public supermercado(String pedido){
+        this.pedido=pedido;
+    }
+    public String isItem(int i){
+        this.item=pedido[i];
+        return pedido[i];
+    }
+    public double preço(){
+
+    }
+    public int estoque(){
+
+    }
+}
