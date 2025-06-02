a51d3f7e36b3d1c7850a6b14566a4b93fc0d31b5
Ppedrocoder <rpedroricardo87@gmail.com>
2025-06-02 20:34:54 +0000

    disciplina

---
+++ disciplina.java
@@ -0,0 +1,55 @@
+public class disciplina{
+    private String nome;
+    private int n1,n2,pf;
+    public disciplina(String nome){
+        this.n1=0;
+        this.n2=0;
+        this.pf=0;
+        this.nome=nome;
+    }
+    public disciplina(String nome,int nota1,int nota2, int pf){
+        this.nome=nome;
+        this.n1=nota1;
+        this.n2=nota2;
+        this.pf= pf;
+    }
+    public int getNota1(){
+        return this.n1;
+    }
+    public int getNota2(){
+        return this.n2;
+    }
+    public int getPf(){
+        return this.pf;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public setNota1(int nota){
+        if (nota>0 | nota<=100){
+            this.n1=nota;
+        }
+    }
+    public setNota2(int nota){
+        this.n2=nota;
+    }
+    public setPf(int nota){
+        this.pf=nota;
+    }
+    public setNome(String nome){
+        this.nome=nome;
+    }
+    private int media(int n1,int n2){
+        return (2*n1+3*n2)/5;
+    }
+    public int mediaParcial(){
+        return media(this.n1, this.n2);
+    }
+    public int mediaFinal(){
+        if (mediaParcial()>=60) {
+            return mediaParcial();
+        }
+        int media= (mediaParcial()+this.pf)/2;
+        return media;
+    }
+}
