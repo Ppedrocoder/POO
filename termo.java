d61351ba89307a0ac5cb0620e3ed81dd4dbb626c
Ppedrocoder <rpedroricardo87@gmail.com>
2025-06-02 21:00:17 +0000

    termo

---
+++ termo.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class termo{
+    private String termo;
+    private int maxTentativas=7;
+    private int tentativas;
+    private boolean[] letras;
+    private boolean finalizado;
+}
